1bc0a543895248dc59cd7e2f00238e0fa74d8cde
RSTZZZ <zachary971011@gmail.com>
2021-06-16 22:36:06 -0400

    update websites

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -1,42 +1,60 @@
 function init_table(options) {
-
   options = options || {};
   var csv_path = options.csv_path || "";
-  var el = options.element || "table-container";
-  var allow_download = options.allow_download || false;
   var csv_options = options.csv_options || {};
-  var datatables_options = options.datatables_options || {};
-
-  $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
 
   $.when($.get(csv_path)).then(
-    function(data){
+    (data) => {
       var csv_data = $.csv.toArrays(data, csv_options);
+      load_table(options, csv_data);
+    },
+    () => {
+      var csv_data = [["Sentiment", "User", "Tweet Text"]];
+      load_table(options, csv_data);
+    }
+  );
+}
 
-      var table_head = "<thead><tr>";
+function load_table(options, csv_data) {
+  options = options || {};
+  var el = options.element || "table-container";
+  var allow_download = options.allow_download || false;
+  var datatables_options = options.datatables_options || {};
 
-      for (head_id = 0; head_id < csv_data[0].length; head_id++) { 
-        table_head += "<th>" + csv_data[0][head_id] + "</th>";
-      }
+  var table_head = "<thead><tr>";
 
-      table_head += "</tr></thead>";
-      $('#my-table').append(table_head);
-      $('#my-table').append("<tbody></tbody>");
+  $("#" + el).html(
+    "<table class='table table-striped table-condensed' id='my-table'></table>"
+  );
 
-      for (row_id = 1; row_id < csv_data.length; row_id++) { 
-        var row_html = "<tr>";
+  for (var head_id = 0; head_id < csv_data[0].length; head_id++) {
+    table_head += "<th>" + csv_data[0][head_id] + "</th>";
+  }
 
-          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
-          }
-          
-        row_html += "</tr>";
-        $('#my-table tbody').append(row_html);
-      }
+  table_head += "</tr></thead>";
+  $("#my-table").append(table_head);
+  $("#my-table").append("<tbody></tbody>");
 
-      $("#my-table").DataTable(datatables_options);
+  for (row_id = 1; row_id < csv_data.length; row_id++) {
+    var row_html = "<tr>";
 
-      if (allow_download)
-        $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
-    });
+    for (col_id = 0; col_id < csv_data[row_id].length; col_id++) {
+      row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+    }
+
+    row_html += "</tr>";
+    $("#my-table tbody").append(row_html);
+  }
+
+  $("#my-table").DataTable(datatables_options);
+
+  if (allow_download) {
+    $("#" + el).append(
+      "<p><a class='btn btn-info' href='" +
+        csv_path +
+        "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>"
+    );
+  }
+
+  $("#loading").hide();
 }
